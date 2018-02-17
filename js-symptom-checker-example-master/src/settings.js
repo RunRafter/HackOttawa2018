6499a659282f738eca83d282c48d6f6a227fcd17
Sean Wu <seanwuyulun@gmail.com>
2018-02-17 17:18:52 -0500

    Add files via upload

---
+++ js-symptom-checker-example-master/src/settings.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Tomasz Gabrysiak @ Infermedica on 06/03/2017.
+ *
+ * Please provide your own app-id and app-key below.
+ * To obtain keys please register at https://developer.infermedica.com.
+ */
+
+const settings = {
+  'app-id': '16e79581',
+  'app-key': 'b14a2bfab21d89c6e1867b34303eb890'
+};
+
+export default settings;
